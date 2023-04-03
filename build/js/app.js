a8e09fb02f7d081b3e4dc72a1ff922b2a77f2791
ASAA19971a <abel.2833@hotmail.com>
2023-04-02 21:32:36 -0500

    new portfolio

--- build/js/app.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",()=>{const e=document.querySelector(".contenedor-loader");e.style.opacity=0,e.style.visibility="hidden"});const typed=new Typed(".typed",{strings:["Programador","Desarrollador Web"],typeSpeed:75,startDelay:300,backSpeed:75,smartBackspace:!0,shuffle:!1,backDelay:1500,loop:!0,loopCount:!1,showCursor:!0,cursorChar:"|",contentType:"html"}),btnMovil=document.querySelector(".menuMovil"),menu=document.querySelector(".menu");btnMovil.addEventListener("click",()=>{menu.classList.toggle("aparecer")});
-//# sourceMappingURL=app.js.map
